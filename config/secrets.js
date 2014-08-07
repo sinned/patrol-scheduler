d7293cab12310bb180b92d57c4ace46ace05233f
Dennis Yang <sinned@gmail.com>
2014-08-07 09:35:11 -0700

    oops URI not URL

--- config/secrets.js
+++ config/secrets.js
@@ -18,7 +18,7 @@
 
 module.exports = {
 
-  db: process.env.MONGODB||process.env.MONGOLAB_URL|| 'mongodb://localhost:27017/test',
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test',
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
